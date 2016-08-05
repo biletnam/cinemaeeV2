9fbfb9464262f0c4281383bfde0c3d1369ec76d1
edalx <edd_alex.66@hotmail.com>
2016-08-05 13:08:24 -0500

    Add menu

--- target/generated-sources/annotations/com/edalx/model/Cine_.java
+++ target/generated-sources/annotations/com/edalx/model/Cine_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-08-05T00:11:16")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-08-05T11:19:31")
 @StaticMetamodel(Cine.class)
 public class Cine_ { 
 
