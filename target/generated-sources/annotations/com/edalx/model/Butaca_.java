6d492c361680cb29d6da92bea64f267db3bf2806
edalx <edd_alex.66@hotmail.com>
2016-08-05 00:38:13 -0500

    Modify index

--- target/generated-sources/annotations/com/edalx/model/Butaca_.java
+++ target/generated-sources/annotations/com/edalx/model/Butaca_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-07-28T23:11:51")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-08-05T00:11:16")
 @StaticMetamodel(Butaca.class)
 public class Butaca_ { 
 
--- target/generated-sources/annotations/com/edalx/model/Funcion_.java
+++ target/generated-sources/annotations/com/edalx/model/Funcion_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-07-28T23:11:51")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-08-05T00:11:16")
 @StaticMetamodel(Funcion.class)
 public class Funcion_ { 
 
